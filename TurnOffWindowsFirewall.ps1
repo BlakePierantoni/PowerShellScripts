799d8695b3200c57d31b4e8df5d57a1300c41741
Blake Pierantoni <46404135+BlakePierantoni@users.noreply.github.com>
2019-12-04 08:59:28 -0500

    Add files via upload

---
+++ TurnOffWindowsFirewall.ps1
@@ -0,0 +1,8 @@
+﻿####################################################################
+#Script: Turn Off Windows Firewall
+#Date: 10/29/2019
+#Author: Blake Pierantoni
+#Description: Turn off Windows Domain, Public, and Private Firewall
+####################################################################
+Write-Host "Turning Off Windows Firewall: Domain, Public, Private"
+Set-NetFirewallProfile -Profile domain,public,private -Enabled false
